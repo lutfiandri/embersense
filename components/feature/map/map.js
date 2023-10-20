d1978aaa9aee4ae2c129b8a6bbd85e4fa8d4d462
lutfiandri <lutfiandri232@gmail.com>
2023-10-20 17:03:02 +0700

    feat(map): remove close button in marker popup

--- components/feature/map/map.js
+++ components/feature/map/map.js
@@ -1,4 +1,10 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import {
+  CircleMarker,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
@@ -14,11 +20,21 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+        <CircleMarker
+          center={[51.505, -0.09]}
+          radius={10}
+          color="red"
+          fillColor="red"
+          fillOpacity={0.7}
+          eventHandlers={{
+            mouseover: (event) => event.target.openPopup(),
+            mouseout: (event) => event.target.closePopup(),
+          }}
+        >
+          <Popup autoClose closeButton={true}>
+            Device Id: ....
           </Popup>
-        </Marker>
+        </CircleMarker>
       </MapContainer>
     </div>
   );
