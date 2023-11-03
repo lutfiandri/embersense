dbdb55a0bdef1eb9217b6b4192c04a508a9cc830
lutfiandri <lutfiandri232@gmail.com>
2023-11-03 23:20:34 +0700

    feat: decompose from upsert sensor to insert and update

--- services/sensor.js
+++ services/sensor.js
@@ -1,6 +1,27 @@
 import { db } from './firebase';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
-export const upsertSensor = async (data) => {
+export const insertSensor = async (data) => {
+  if (getSensor(data.sensorId)) {
+    throw new Error(`Sensor ${data.sensorId} telah ada di database.`);
+  }
   await setDoc(doc(db, 'sensors', data.sensorId), data);
+};
+
+export const updateSensor = async (data) => {
+  if (!getSensor(data.sensorId)) {
+    throw new Error(`Sensor ${data.sensorId} tidak ditemukan di database.`);
+  }
+  await setDoc(doc(db, 'sensors', data.sensorId), data);
+};
+
+export const getSensor = async (id) => {
+  const docRef = doc(db, 'sensors', id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return docSnap;
+  } else {
+    return null;
+  }
 };
