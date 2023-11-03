468439a4fbbdfc2066f41e38cace0c59cc7ced02
lutfiandri <lutfiandri232@gmail.com>
2023-11-03 23:32:29 +0700

    fix: missing await

--- services/sensor.js
+++ services/sensor.js
@@ -2,14 +2,16 @@ import { db } from './firebase';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 export const insertSensor = async (data) => {
-  if (getSensor(data.sensorId)) {
+  const sensor = await getSensor(data.sensorId);
+  if (sensor) {
     throw new Error(`Sensor ${data.sensorId} telah ada di database.`);
   }
   await setDoc(doc(db, 'sensors', data.sensorId), data);
 };
 
 export const updateSensor = async (data) => {
-  if (!getSensor(data.sensorId)) {
+  const sensor = await getSensor(data.sensorId);
+  if (!sensor) {
     throw new Error(`Sensor ${data.sensorId} tidak ditemukan di database.`);
   }
   await setDoc(doc(db, 'sensors', data.sensorId), data);
@@ -20,7 +22,7 @@ export const getSensor = async (id) => {
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    return docSnap;
+    return docSnap.data();
   } else {
     return null;
   }
