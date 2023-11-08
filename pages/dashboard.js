5518ebfcb6e41427f4d432169703b46af8385cf2
lutfiandri <lutfiandri232@gmail.com>
2023-11-08 12:29:22 +0700

    fix: ssr problem

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,9 @@
-import DefaultLayout from "@/components/layout/DefaultLayout";
-import Table from "@/components/feature/tabel/table";
+import DefaultLayout from '@/components/layout/DefaultLayout';
+import dynamic from 'next/dynamic';
+
+const Table = dynamic(() => import('@/components/feature/tabel/table'), {
+  ssr: false,
+});
 
 export default function Dashboard() {
   return (
