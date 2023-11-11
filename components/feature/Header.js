63317035755ea5622c32d35dc983f858eda1ad06
kurniakdu <kurniakdu@gmail.com>
2023-11-11 15:05:13 +0700

    update about page

--- components/feature/Header.js
+++ components/feature/Header.js
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="h-10 text-center w-full grid place-items-center bg-slate-200 shadow-sm sticky top-0">
+    <div className="h-10 text-center text-white font-bold w-full grid place-items-center bg-[#252422] shadow-sm sticky top-0">
       EMBERSENSE
     </div>
   );
