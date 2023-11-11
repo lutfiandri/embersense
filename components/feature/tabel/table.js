27777f2a7fdf4ecd68e6e17e537303e4d17bffef
sitimalatania <sitimalatania@mail.ugm.ac.id>
2023-11-11 21:41:58 +0700

    feat: fix delete function

--- components/feature/tabel/table.js
+++ components/feature/tabel/table.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import DataTable from 'react-data-table-component';
-import { TiArrowSortedDown } from 'react-icons/ti';
-import DataTableExtensions from 'react-data-table-component-extensions';
-import 'react-data-table-component-extensions/dist/index.css';
-import { collection, getDocs, deleteDoc, updateDoc } from 'firebase/firestore'; // Import Firestore functions
-import { FaTrash } from 'react-icons/fa';
-import { Modal, Button } from 'antd'; // Import Modal and Button from Ant Design
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import { TiArrowSortedDown } from "react-icons/ti";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import { collection, onSnapshot, deleteDoc } from "firebase/firestore"; // Import Firestore functions
+import { FaTrash } from "react-icons/fa";
+import { Modal, notification } from "antd"; // Import Modal and notification from Ant Design
 
-import { db } from '@/services/firebase';
+import { db } from "@/services/firebase";
+import { deleteSensor } from "@/services/sensor";
 
 const formatDateSeconds = (seconds) => {
-  try {
-    const date = new Date(seconds * 1000 || 0);
-    const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-    };
+  const date = new Date(seconds * 1000 || 0);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
 
-    const formatter = new Intl.DateTimeFormat('id-ID', options);
-    const formattedDate = formatter.format(date);
-    return formattedDate;
-  } catch {
-    return '';
-  }
+  const formatter = new Intl.DateTimeFormat("id-ID", options);
+  const formattedDate = formatter.format(date);
+  return formattedDate;
 };
 
 const Table = () => {
@@ -33,15 +30,17 @@ const Table = () => {
     columns: [],
     data: [],
   });
+  const [notificationApi, notificationContextHolder] =
+    notification.useNotification();
 
   const [sensorToDelete, setSensorToDelete] = useState(null);
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   useEffect(() => {
     // Fetch data from Firebase
-    const fetchData = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, 'sensors'));
+    const unsubscribe = onSnapshot(
+      collection(db, "sensors"),
+      (querySnapshot) => {
         const data = [];
 
         querySnapshot.forEach((doc) => {
@@ -50,56 +49,56 @@ const Table = () => {
 
         const columns = [
           {
-            name: 'ID sensor',
-            selector: 'sensorId',
+            name: "ID sensor",
+            selector: "sensorId",
             sortable: true,
           },
           {
-            name: 'Nama sensor',
-            selector: 'sensorName',
+            name: "Nama sensor",
+            selector: "sensorName",
             sortable: true,
           },
           {
-            name: 'Latitude',
-            selector: 'latitude',
+            name: "Latitude",
+            selector: "latitude",
             sortable: true,
           },
           {
-            name: 'Longitude',
-            selector: 'longitude',
+            name: "Longitude",
+            selector: "longitude",
             sortable: true,
           },
           {
-            name: 'Waktu Created',
-            selector: 'createdAt.seconds',
+            name: "Waktu Created",
+            selector: "createdAt.seconds",
             sortable: true,
-            format: (row) => formatDateSeconds(row?.createdAt?.seconds),
+            format: (row) => formatDateSeconds(row.createdAt.seconds),
           },
           {
-            name: 'Waktu Mati',
-            selector: 'updatedAt.seconds',
+            name: "Waktu Mati",
+            selector: "updatedAt.seconds",
             sortable: true,
             format: (row) => {
-              const createdAt = row?.createdAt?.seconds;
-              const updatedAt = row?.updatedAt?.seconds;
+              const createdAt = row.createdAt.seconds;
+              const updatedAt = row.updatedAt.seconds;
 
               if (updatedAt !== createdAt) {
-                return formatDateSeconds(updatedAt); // Tampilkan waktu mati sesuai waktu update
+                return formatDateSeconds(updatedAt);
               } else {
-                return 'Sensor Masih Hidup';
+                return "Sensor Masih Hidup";
               }
             },
           },
           {
-            name: 'Action',
+            name: "Action",
             sortable: false,
             cell: (row) => (
               <FaTrash
                 type="primary"
                 danger
                 onClick={() => {
-                  setSensorToDelete(row); // Menandai sensor yang akan dihapus
-                  setShowDeleteConfirmation(true); // Tampilkan modal konfirmasi
+                  setSensorToDelete(row);
+                  setShowDeleteConfirmation(true);
                 }}
               >
                 Hapus Sensor
@@ -109,33 +108,35 @@ const Table = () => {
         ];
 
         setTableData({ columns, data });
-      } catch (error) {
-        console.error('Error fetching data:', error);
       }
-    };
+    );
 
-    fetchData();
+    return () => {
+      // Unsubscribe from the snapshot listener when the component unmounts
+      unsubscribe();
+    };
   }, []);
 
   const handleDelete = async () => {
     if (sensorToDelete) {
-      const sensorRef = collection(db, 'sensors').doc(sensorToDelete.id);
-
       try {
-        await deleteDoc(sensorRef);
-        console.log('Sensor berhasil dihapus');
-        // Setelah menghapus sensor, Anda dapat memuat ulang data atau memperbarui state data lokal Anda.
+        await deleteSensor(sensorToDelete.sensorId);
+        notificationApi.success({
+          message: "Berhasil",
+          description: `Sensor ${sensorToDelete.sensorName} berhasil ditambahkan`,
+        });
       } catch (error) {
-        console.error('Error menghapus sensor:', error);
+        console.error("Error menghapus sensor:", error);
       }
 
-      setSensorToDelete(null); // Reset sensorToDelete setelah penghapusan
-      setShowDeleteConfirmation(false); // Tutup modal konfirmasi
+      setSensorToDelete(null);
+      setShowDeleteConfirmation(false);
     }
   };
 
   return (
     <div className="py-10 text-center shadow-xl">
+      {notificationContextHolder}
       <DataTableExtensions {...tableData} print={false}>
         <DataTable
           columns={tableData.columns}
